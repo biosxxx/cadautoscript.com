cb87c4ed11c6ba16d7c7bf84041e92d2e3117600
Yurii M <milieninyv@gmail.com>
2025-12-03 20:41:15 +0200

    Register custom search navbar item

--- src/theme/NavbarItem/ComponentTypes.tsx
+++ src/theme/NavbarItem/ComponentTypes.tsx
@@ -1,7 +1,9 @@
 import ComponentTypes from '@theme-original/NavbarItem/ComponentTypes';
+import CustomSearchNavbarItem from '@site/src/theme/NavbarItem/CustomSearch';
 import NavbarAuthNavbarItem from '@site/src/theme/NavbarItem/NavbarAuth';
 
 export default {
   ...ComponentTypes,
+  'custom-search': CustomSearchNavbarItem,
   'custom-navbar-auth': NavbarAuthNavbarItem,
 };
